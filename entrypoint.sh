5227f6cffd0732cf15b56f1ea70af0e85d7bc67f
Ido Halevi <32218210+idohalevi@users.noreply.github.com>
2018-07-09 12:54:21 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-if test -z $FLUENTD_CONFIG_URI; then
-  echo "Using default fluent.conf"
-else
-  echo "Pulling flent.conf from $FLUENTD_CONFIG_URI"
-  wget $FLUENTD_CONFIG_URI -O /fluentd/etc/fluent.conf
-fi
-exec fluentd -c /fluentd/etc/${FLUENTD_CONF} --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
